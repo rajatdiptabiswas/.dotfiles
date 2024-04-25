5d35765a317116aeabeb8d3e86a13e7d40303127
rajat <rdb5@sfu.ca>
2024-04-25 10:39:17 -0700

    Create setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y git stow
+
+rm -rf "$HOME/.dotfiles/"
+git clone --branch linux-apt-bash https://github.com/rajatdiptabiswas/.dotfiles.git "$HOME/.dotfiles"
+
+DOTFILES=("$HOME/.tmux.conf" "$HOME/.vimrc")
+
+for DOTFILE in "${DOTFILES[@]}"; do
+    rm -f "$DOTFILE"
+done
+
+stow --dir "$HOME/.dotfiles" --target "$HOME" .
