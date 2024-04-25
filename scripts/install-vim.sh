ccad418af1aa8ec35db9cdb8a9e3f01c5d7d938e
rajat <rdb5@sfu.ca>
2024-04-25 10:39:23 -0700

    Update install-vim.sh

--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -1,4 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y curl vim xclip
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
