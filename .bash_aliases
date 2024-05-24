37295675abe255be37bb62899036504711d6245d
rajat <rdb5@sfu.ca>
2024-05-24 15:39:28 -0700

    Create .bash_aliases

---
+++ .bash_aliases
@@ -0,0 +1,5 @@
+# ls
+alias l='ls -ACF'
+alias ll='ls -lAhF'
+alias lt='ls -lAhFt'
+alias ltr='ls -lAhFtr'
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ sudo apt install -y git stow
 rm -rf "$HOME/.dotfiles/"
 git clone --branch linux-apt-bash https://github.com/rajatdiptabiswas/.dotfiles.git "$HOME/.dotfiles"
 
-DOTFILES=("$HOME/.tmux.conf" "$HOME/.vimrc")
+DOTFILES=("$HOME/.tmux.conf" "$HOME/.vimrc" "$HOME/.bash_aliases")
 
 for DOTFILE in "${DOTFILES[@]}"; do
     rm -f "$DOTFILE"
