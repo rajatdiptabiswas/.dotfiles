10d4cb1e14b8997c3f78f907e92694d976b123b7
rajat <rdb5@sfu.ca>
2024-07-12 15:58:50 -0700

    Create install-locale.sh
    Set locale to en_US.UTF-8

---
+++ scripts/install-locale.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install locales
+
+sudo locale-gen en_US.UTF-8
+
+echo "export LANG=en_US.UTF-8" >> "$HOME/.bashrc"
+echo "export LC_ALL=en_US.UTF-8" >> "$HOME/.bashrc"
