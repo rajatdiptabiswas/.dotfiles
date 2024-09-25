4c56de91d8455dc4162ab5d5f74d2fe481de6cca
rajat <rdb5@sfu.ca>
2024-09-25 15:35:22 -0700

    Update install-font.sh

--- scripts/install-font.sh
+++ scripts/install-font.sh
@@ -26,7 +26,7 @@ for font in "${fonts[@]}"; do
     download_url="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$zip_file"
     wget -O "/tmp/$zip_file" "$download_url"
     unzip "/tmp/$zip_file" -d "/tmp/$font/"
-    mv "/tmp/$font/*.ttf" "$fonts_dir"
+    mv "/tmp/$font/"*.ttf "$fonts_dir"
     rm "/tmp/$zip_file"
     rm -rf "/tmp/$font/"
 done
