168f27f0ecc678dfe86452f640adbc3e09a9abe3
rajat <rdb5@sfu.ca>
2024-10-01 13:38:26 -0700

    Update install-tmux.sh

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -2,3 +2,8 @@
 
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+tmux source-file ~/.tmux.conf
+
+# install tmux plugins
+$HOME/.tmux/plugins/tpm/bin/install_plugins
