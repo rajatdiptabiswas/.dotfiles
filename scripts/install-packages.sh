08257ba40b1c5ef82612e3c6e441905e2784ca8a
rajat <rdb5@sfu.ca>
2024-10-30 17:44:40 -0700

    Create install-packages.sh

---
+++ scripts/install-packages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y \
+    build-essential \
+    ca-certificates \
+    curl \
+    dnsutils \
+    ethtool \
+    fontconfig \
+    gcc \
+    gdb \
+    git \
+    grep \
+    htop \
+    iproute2 \
+    iptables \
+    iputils-ping \
+    locales \
+    lsb-release \
+    make \
+    man-db \
+    ncdu \
+    netcat \
+    nmap \
+    openssl \
+    perl \
+    psmisc \
+    python3 \
+    python3-dev \
+    python3-pip \
+    python3-setuptools \
+    python3-wheel \
+    software-properties-common \
+    stow \
+    sudo \
+    tar \
+    tcpdump \
+    tmux \
+    traceroute \
+    tree \
+    unzip \
+    valgrind \
+    vim \
+    wget \
+    xclip
