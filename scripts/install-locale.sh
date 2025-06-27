6f2ee59780a67cb1b8f3b3de957a44e61695bceb
rajat <rdb5@sfu.ca>
2025-06-27 13:45:08 -0400

    Remove files

--- scripts/install-locale.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install -y locales
-
-sudo locale-gen en_US.UTF-8
-
-echo "export LANG=en_US.UTF-8" >> "$HOME/.bashrc"
-echo "export LC_ALL=en_US.UTF-8" >> "$HOME/.bashrc"
--- scripts/install-vim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install -y curl vim xclip
-
-# vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +'PlugInstall --sync' +qa
