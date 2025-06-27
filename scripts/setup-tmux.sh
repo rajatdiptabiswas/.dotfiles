3ef3f56eaac6656ca66be049a70af01b12c78b6b
rajat <rdb5@sfu.ca>
2025-06-27 13:48:52 -0400

    Create setup-tmux.sh

---
+++ scripts/setup-tmux.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+tmux source-file ~/.tmux.conf
+
+# install tmux plugins
+$HOME/.tmux/plugins/tpm/bin/install_plugins
